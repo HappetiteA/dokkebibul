66496271081f577b1c9059f32c11f451a88ad192
1n1tial <dan.yun0821@gmail.com>
2025-09-08 14:53:24 +0900

    feat: useAuth hook for login and logout

--- app.config.js
+++ app.config.js
@@ -7,7 +7,7 @@ export default {
     "version": "1.0.0",
     "orientation": "portrait",
     "icon": "./assets/images/icon.png",
-    "scheme": "snuhappetitedokkaebibul",
+    "scheme": process.env.APP_SCHEME,
     "userInterfaceStyle": "automatic",
     "newArchEnabled": true,
     "splash": {
@@ -18,7 +18,7 @@ export default {
     "ios": {
       "supportsTablet": true,
       "usesAppleSignIn": true,
-      "bundleIdentifier": "com.1n1tial.snuhappetitedokkaebibul",
+      "bundleIdentifier": process.env.IOS_BUNDLE_ID,
       "infoPlist": {
         "ITSAppUsesNonExemptEncryption": false
       }
@@ -30,11 +30,11 @@ export default {
       },
       "config": {
         "googleSignIn": {
-          "certificateHash": "20:C7:27:42:EF:9D:A2:78:0D:E6:67:19:36:ED:A9:1A:8B:F1:0A:7C"
+          "certificateHash": process.env.ANDROID_GOOGLE_SHA1
         }
       },
       "edgeToEdgeEnabled": true,
-      "package": "com.dokkaebibul.snu_happetite"
+      "package": process.env.ANDROID_PACKAGE
     },
     "web": {
       "bundler": "metro",
@@ -48,7 +48,7 @@ export default {
       [
         "@react-native-google-signin/google-signin",
         {
-          "iosUrlScheme": "com.googleusercontent.apps.537703566025-31nn1n7g9rn5edmuddu20o64d97ugkqm"
+          "iosUrlScheme": process.env.IOS_URL_SCHEME
         }
       ],
       [
@@ -70,7 +70,7 @@ export default {
     "extra": {
       "router": {},
       "eas": {
-        "projectId": "54157097-feaf-4da9-a259-32b5eab2352b"
+        "projectId": process.env.EAS_PROJECT_ID
       }
     },
     "owner": "happetitea"
