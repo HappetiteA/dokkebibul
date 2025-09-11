4f797a5f3ec3f98622b80c263700e78c85be6be0
1n1tial <dan.yun0821@gmail.com>
2025-09-11 14:08:29 +0900

    feat: profile insert logic to onboarding

--- app/(app)/onboarding.tsx
+++ app/(app)/onboarding.tsx
@@ -1,22 +1,44 @@
 import { Text, View, Button, Alert, TouchableOpacity } from "react-native";
 import { useRouter } from "expo-router";
+import { useAuth } from "@/utils/AuthContext";
 import { supabase } from "@/utils/supabase";
 
 export default function OnBoarding() {
   const router = useRouter();
+  const { session, user, profile } = useAuth();
+
   const handleOnboarding = async () => {
-    const {
-      data: { session },
-      error: sessionError,
-    } = await supabase.auth.getSession();
-    if (!session || sessionError || !session.user) {
-      Alert.alert("Error", "User session not found, redirecting to login...");
+    if (!user || !user.id || !session) {
+      console.log("User session missing");
+      Alert.alert("User session missing, please sign in again");
       router.replace("/(auth)/sign-in");
       return;
     }
-    const user = session.user;
-    router.replace("/(app)/(tabs)/(home)");
-  };
+    if (profile) {
+      console.log("User profile already exists in DB");
+      Alert.alert("User profile already exists in DB");
+      router.replace("/(app)/(tabs)/(home)");
+      return;
+    }
+    try {
+      const { error } = await supabase
+        .from("profiles")
+        .insert(
+          {
+            user_id: user.id,
+            name: 'a',
+          },
+        );
+      if (error) {
+        console.log(error);
+        Alert.alert(`Failed to insert profile to DB: ${error}`);
+      }
+    } catch (err: any) {
+      console.log(err);
+      Alert.alert(`Failed to insert profile to DB: ${err}`);
+    }
+  }
+  
   return (
     <View>
       <Text>Hello Onboarding</Text>
