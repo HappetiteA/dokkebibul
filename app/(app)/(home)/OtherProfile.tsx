6ac884f14fcf634952914037007ae0ea78946f58
dbrldus <rldus0864@gmail.com>
2025-11-10 19:21:51 +0900

    Revert "fix: link OtherProfile with chat screen again"
    This reverts commit 55cbb95a83f5332cd11dd43c8f2eace6746edbcd.

--- app/(app)/(home)/OtherProfile.tsx
+++ app/(app)/(home)/OtherProfile.tsx
@@ -4,7 +4,7 @@ import headerStyle from "@/components/style/headerStyle";
 import { useEffect, useRef, useState } from "react";
 import { supabase } from "@/utils/supabase";
 import { PostgrestError } from "@supabase/supabase-js";
-import { getProfileById, getFollowings, getChatRooms } from "@/hooks/data";
+import { getProfileById, getFollowings } from "@/hooks/data";
 import ModalChain, { ModalChainRef } from "@/components/ModalChain";
 import { useAuth } from "@/utils/AuthContext";
 
@@ -15,12 +15,12 @@ interface IProfile {
 
 export default function OtherProfileScreen() {
   const router = useRouter();
-  const { profile: myProfile } = useAuth();
+  const { profile } = useAuth();
 
   const params = useLocalSearchParams();
-  const otherUserId = params.user_id as string;
+  const user_id = params.user_id as string;
 
-  const [otherProfile, setOtherProfile] = useState<IProfile>();
+  const [userInfo, setUserInfo] = useState<IProfile>();
 
   const [follow, setFollow] = useState(false);
 
@@ -30,9 +30,9 @@ export default function OtherProfileScreen() {
 
   useEffect(() => {
     (async () => {
-      const data = await getProfileById(otherUserId);
-      if (data == null) return;
-      setOtherProfile(data);
+      const profile = await getProfileById(user_id);
+      if (profile == null) return;
+      setUserInfo(profile);
     })();
 
     (async () => {
@@ -42,12 +42,12 @@ export default function OtherProfileScreen() {
         return;
       }
       followingsData.forEach((following) => {
-        if (following.dst_id === otherUserId) {
+        if (following.dst_id === user_id) {
           setFollow(true);
         }
       });
     })();
-  }, [otherUserId]);
+  }, [user_id]);
 
   const onFollowBtnPressed = async () => {
     setFollowBtnEnabled(false);
@@ -83,53 +83,22 @@ export default function OtherProfileScreen() {
   };
 
   const onChatBtnPressed = async () => {
-    if (myProfile == null) return;
+    if (typeof user_id !== "string") return;
+    if (profile == null) return;
 
-    // CHECK LOGIC, is update_conv_chat_enabled is enough for chatting?
     const { error } = await supabase.rpc("update_conversations_chat_enabled", {
-      u1id: myProfile.user_id,
-      u2id: otherUserId,
+      u1id: profile.user_id,
+      u2id: user_id,
       new_chat_enabled: true,
     });
-
-    // temp code.
-    var conversation_id: string = "";
-    const chatList = await getChatRooms();
-    chatList?.forEach((value) => {
-      var user1_id = myProfile.user_id;
-      var user2_id = otherUserId;
-      if (user2_id < user1_id) {
-        var temp = user2_id;
-        user2_id = user1_id;
-        user1_id = temp;
-      }
-
-      if (value.user1_id == user1_id && value.user2_id == user2_id) {
-        conversation_id = value.id;
-      }
-    });
-    //
-
-    var user_ids = JSON.stringify([myProfile.user_id, otherUserId]);
-    var user_names = JSON.stringify([myProfile.name, otherProfile?.name]);
-
-    if (conversation_id != "") {
-      router.navigate({
-        pathname: "/chat/[id]",
-        params: {
-          id: conversation_id,
-          user_ids: user_ids,
-          user_names: user_names,
-        },
-      });
-    }
+    router.navigate({ pathname: "/chat/[id]", params: { id: user_id } });
   };
 
   return (
     <View>
       <OtherProfileScreenHeader />
       <View>
-        <Text>{otherProfile?.name}</Text>
+        <Text>{userInfo?.name}</Text>
         <Text>Location</Text>
         <Text>Description</Text>
         <Text>Account Info</Text>
@@ -160,7 +129,7 @@ export default function OtherProfileScreen() {
           {
             children: (
               <View>
-                <Text>{otherProfile?.name}님을 차단하시겠습니까?</Text>
+                <Text>{userInfo?.name}님을 차단하시겠습니까?</Text>
                 <TouchableOpacity
                   onPressOut={blockModalChainRef.current?.close}
                 >
@@ -177,7 +146,7 @@ export default function OtherProfileScreen() {
           {
             children: (
               <View>
-                <Text>{otherProfile?.name}님을 차단했습니다</Text>
+                <Text>{userInfo?.name}님을 차단했습니다</Text>
                 <TouchableOpacity
                   onPressOut={blockModalChainRef.current?.goNext}
                 >
