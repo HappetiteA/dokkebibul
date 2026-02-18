def9a7f5672c3c295687d858f70a3b18d79b279f
dbrldus <rldus0864@gmail.com>
2026-02-19 03:23:30 +0900

    fix : error message

--- app/(app)/(home)/MyProfile.tsx
+++ app/(app)/(home)/MyProfile.tsx
@@ -132,7 +132,7 @@ export default function MyProfileScreen() {
     );
 
     if (conversationError) {
-      console.log(conversationError.message);
+      console.log("Self Chat Error : ", conversationError.message);
       return;
     }
 
